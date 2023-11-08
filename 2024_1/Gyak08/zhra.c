3f283cb925a0104b4b43a3527fcad90134f218ec
Daerile <palldominiko8@gmail.com>
2023-11-08 11:22:21 +0100

    Tudnivalok file

---
+++ 2024_1/Gyak08/zhra.c
@@ -0,0 +1,68 @@
+// includeok, amik kellhetnek
+
+#include <stdio.h> // Input olvasás, output írás.
+#include <stdbool.h> // bool változótípus
+#include <math.h> // Matematikai függvények, értékek pl. pow
+#include <ctype.h> // Karakterkezeléshez hasznos függvények toupper(), isupper() ...
+
+// Random szám generálásához:
+#include <time.h>
+#include <stdlib.h>
+
+int main(){
+    
+    // Mit kell tudni:
+
+    // 1. óra
+    /*
+    Alaptípusok: int, float, double, char, bool
+    Inicializálni, deklarálni ezeket.
+    Kiiratás: printf(), beolvasás: scanf()
+    Ezekhez a megfelelő type specifier (pl %i, %c, ...) alkalmazása
+    printf() - el formázás.
+    */
+
+    // 2. óra
+    /*
+    ciklusok: for, while, dowhile
+    static kulcsszó: az inicializálástól végig benne van a program életciklusában
+    if - else elágazások
+    sizeof() értése és használata
+    char[]: String, végén '\0' (null) karakter
+    */
+
+    // 3. óra
+    /*
+    makrók
+    switch case
+    unsigned kulcsszó: szám változók, ahol nincs - érték, emiatt 2* annyi + van.
+    <, >, ==, /=, &&, || operátorok
+    random szám generálás
+    */
+
+    // 4. óra
+    /*
+    függvények különböző visszatérési értékekkel és paraméterekkel
+    ctype.h segítségével: karakterfüggvények ismerete
+    getchar()
+    EOF-ig olvasás -- 7. gyakorlat anyagában helyesen van.
+    */
+
+    // 5. óra
+    /*
+    tömbök
+    */
+
+    // 6. óra
+    /*
+    pointerek
+    tömbök pointer aritmetikával, miért jó, stb...
+    */
+
+    // 7. óra
+    /*
+    kétdimenziós tömbök
+    ennek a pointer aritmetikája
+    */
+    return 0;
+}
